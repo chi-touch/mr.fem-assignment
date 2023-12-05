fd12a547e410614398c853897527bea96020c248
ChiChi <deborahdelighted5@gmail.com>
2023-12-04 23:17:18 -0800

    Deleted some files

---
+++ GeoPolitical.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class GeoPolitical{
+
+	public static void main(String[] args){
+	Scanner state = new Scanner(System.in);
+
+	System.out.print("enter state of origin:");
+
+	if(state == Kebbi){
+		System.out.print("NW");
+}
+	if(state == Sokoto){
+		System.out.print("NW");
+}
+	if(state == Kastina){
+		System.out.print("NW");
+
+}
+	if(state == Zamfara){
+		System.out.print("NW");
+}
+
+	if(state == Kaduna){
+		System.out.print("NW");
+}
+	if(state == Kano){
+		System.out.print("NW");
+}
+	if(state == Adamanwa){
+		System.out.print("NE");
+}
+	if(state == Yobe){
+		System.out.print("NE");
+}
+	if(state == Borno){
+		System.out.print("NE");
+}
+	if(state == Bauchi){
+		System.out.print("NE");
+}
+	if(state == Gombe){
+		System.out.print("NE");
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
